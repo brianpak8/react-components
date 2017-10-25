841242196717879ef442955e0a80f2506a91120b
Brian Pak <brianpak27@gmail.com>
2017-10-25 12:11:21 -0700

    complete, but cannot bold on hover, instead can crossout on click

--- app.js
+++ app.js
@@ -1,1 +1,76 @@
-// TODO
+// // TODO
+// //
+// var GroceryList = () => (
+//   <ul>
+//     <li>bbq sauce</li>
+//     <li>ketchup</li>
+//     <Ranch />
+//     <Mustard />
+//
+//   </ul>
+// )
+//
+//
+// var Ranch = () => (
+//   <li>ranch</li>
+// );
+//
+// var Mustard = () => (
+//   <li>mustard</li>
+// );
+//
+// ReactDOM.render(<GroceryList />, document.getElementById('app'));
+//
+// var GroceryListItem = (props) => (
+//   <ul>
+//     <li>{props.item[0]}</li>
+//     <li>{props.item[1]}</li>
+//     <li>{props.item[2]}</li>
+//   </ul>
+// );
+// var GroceryList = () => (
+//   <div>
+//     <GroceryListItem item={['butter', 'bread', 'cheese']} />
+//   </div>
+//
+// );
+//
+// ReactDOM.render(<GroceryList />, document.getElementById('app'));
+
+
+
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+    return <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+  }
+}
+
+
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item =>
+    <GroceryListItem item={item} />
+  )}
+  </ul>
+
+);
+
+
+
+ReactDOM.render(<GroceryList items={['tea', 'coffee', 'water']}/>, document.getElementById('app'));
